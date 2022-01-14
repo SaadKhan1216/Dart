0f037fdb0527728ade2d661943f2398d9f4a394a
Pakistan-123 <saadkhan1216@gmail.com>
2022-01-14 20:47:22 +0500

    Class 1 (Variables)

---
+++ Variables.dart
@@ -0,0 +1,11 @@
+// SIMPLE VARIABLE DECLARANCE....
+void main() {
+  var a = 'Saad';
+  var b = 20;
+  var c = 22.3;
+  var d = 'github.com/SaadKhan1216';
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+}
