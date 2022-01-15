ca3cfa51ab2a75d7b1cbcfe94cc13d8c10482858
Pakistan-123 <saadkhan1216@gmail.com>
2022-01-15 20:53:46 +0500

    Class 2

---
+++ DataTypes.dart
@@ -0,0 +1,58 @@
+// DATA TYPES ..................
+void main() {
+  // DATA TYPE 'INT'.....
+
+  int a = 20;
+  int b = 30;
+  print(a);
+  print(b);
+
+  // DATA TYPE 'STR....
+
+  String fname = 'Saad';
+  String lname = 'Khan';
+  print(fname);
+  print(lname);
+
+  // DATA TYPE 'DOUBLE'....
+
+  double dNum = 20.22;
+  double simpNum = 20;
+  print(dNum);
+  print(simpNum);
+
+  // DATA TYPE 'NUM'....
+
+  num abc = 20;
+  num xyz = 20.22;
+  print(abc);
+  print(xyz);
+
+  // DATA TYPE 'BOOL'....
+
+  bool cond = true;
+  bool cond1 = false;
+  print(cond);
+  print(cond1);
+
+  // DATA TYPE 'LIST'....
+
+  List lst = [1, 2, 3, 4, 5];
+  print(lst);
+
+  // Type Specified List....
+  List<String> lstStr = ['Saad', '2', '3', 'Khan'];
+  print(lstStr);
+
+  // Dynamic List....
+  List<dynamic> lstDn = [1, 'Saad', '2', 4];
+  print(lstDn);
+
+  // DATA TYPE 'MAP'....
+  Map map = {1: 'Saad'};
+  print(map);
+
+  // Type Specified Map....
+  Map<int, String> map1 = {1: 'Saad'};
+  print(map1);
+}
