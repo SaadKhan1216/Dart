53e8b396165f710cc500c9905cfb13bde0ecc384
Pakistan-123 <saadkhan1216@gmail.com>
2022-01-17 07:41:50 +0500

    Class 3

---
+++ ArithmeticalOperators.dart
@@ -0,0 +1,13 @@
+// ARITHMETIC OPERATORS....
+void main() {
+  int a = 40;
+  int b = 2;
+  int c = 33;
+  print(a + b); // ADDITION
+  print(a - b); // SUBTRACTION
+  print(a * b); // MULTIPLICATION
+  print(a / b); // DIVISION
+  print(c / b); // DIVISION
+  print(c ~/ b); // ROUNDOFF DIVISION
+  print(a % b); // REMAINDER
+}
