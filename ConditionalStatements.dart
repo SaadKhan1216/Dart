9e465510cc01668b52ed4b5c81216cd499ac24fd
Pakistan-123 <saadkhan1216@gmail.com>
2022-01-19 19:05:17 +0500

    If-Elseif-Else

---
+++ ConditionalStatements.dart
@@ -0,0 +1,19 @@
+void main() {
+  int a = 24;
+  int b = 54;
+
+  // IF STATEMENT...........
+  if (a > b) {
+    print('$a is greater than $b');
+  }
+
+  // ELSE IF STATEMENT......
+  else if (b > a) {
+    print('$b is greater than $a');
+  }
+
+  // ELSE STATEMENT.....
+  else {
+    print('Both the Numbers are equal');
+  }
+}
