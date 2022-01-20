b6b90642b15927060c139e4a5a3af8b61dca4886
Pakistan-123 <saadkhan1216@gmail.com>
2022-01-20 20:36:40 +0500

    ConditionsUsingSwitch

---
+++ SwitchCase.dart
@@ -0,0 +1,14 @@
+// SWITCH CASE STATEMENT..........
+void main() {
+  int engineSize = 600;
+  switch (engineSize) {
+    case 500:
+      print('500cc it is.');
+      break;
+    case 600:
+      print('600cc it is');
+      break;
+    default:
+      print('Neither');
+  }
+}
