a7febcc7c6d32d794c39042df7683f4d168c58c1
Pakistan-123 <saadkhan1216@gmail.com>
2022-01-21 18:58:21 +0500

    Class 6

---
+++ RelationalOperators.dart
@@ -0,0 +1,15 @@
+// RELATIONAL OPERATORS.....
+void main() {
+  var a = 2 > 3; // Greater Than
+  var b = 2 < 3; // Less Than
+  var c = 2 <= 2; // Less than and equal to
+  var d = 2 >= 4; // Greater than and equal to
+  var e = 2 == 2; // Equivalency
+  var f = 2 != 4; // Not Equal to
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  print(e);
+  print(f);
+}
