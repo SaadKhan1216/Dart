bd35fd0622b3ef93a93e2f2e37da2a9a27ea33a9
Pakistan-123 <saadkhan1216@gmail.com>
2022-01-25 21:36:15 +0500

    Class9

---
+++ Functions.dart
@@ -0,0 +1,8 @@
+void main() {
+  BringTea(3, 'Hotel', 2);
+  BringTea(4, 'Canteen', 3);
+}
+
+BringTea(int cups, String source, int sugar) {
+  print('$cups cups from $source, $sugar teaspoons sugar');
+}
