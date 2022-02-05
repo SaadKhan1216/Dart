ff36bdf055ce92d9d19daa9c33c4c73189e8ca89
Pakistan-123 <saadkhan1216@gmail.com>
2022-02-05 15:21:57 +0500

    Class14

---
+++ AnonymousFunc.dart
@@ -0,0 +1,12 @@
+// jaha zrurt ho wahin bnaakr use karein.............
+void main() {
+  var sum = (int a, int b) {
+    // Variable mein as a refernce store hua hai function ka naam nhi koi.
+    print(a + b);
+  };
+  sum(10, 20);
+  // List<String> dostLog = ['Saad', 'Ali'];
+  // dostLog.forEach((name) {
+  //   print(name);
+  //  });
+}
